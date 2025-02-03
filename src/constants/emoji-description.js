3d0c85dc66bf0811fad6186aa7a280447ea79e11
Sourabh <sourabhchangmai11@gmail.com>
2025-02-03 11:55:06 +0530

    Initial set up of project

---
+++ src/constants/emoji-description.js
@@ -0,0 +1,7 @@
+export const emojiDescription = {
+  happy: "Happy",
+  sad: "Sad",
+  angry: "Angry",
+  love: "Loving",
+  like: "Like",
+};
---
+++ src/constants/emoji.js
@@ -0,0 +1,7 @@
+export const emoji = {
+  happy: "😊",
+  sad: "😢",
+  angry: "😠",
+  love: "😍",
+  like: "👍",
+};
